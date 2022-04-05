635ef0a88fc5fded780bc2917940e2e2b5e42046
RY4121 <yoshida.rariru@gmail.com>
2022-04-05 17:44:55 +0900

    add col 'password'

--- db/sql/01_create_table.sql
+++ db/sql/01_create_table.sql
@@ -3,6 +3,7 @@ CREATE TABLE `m_users`(
     `id` int NOT NULL AUTO_INCREMENT COMMENT 'ユーザID',
     `user_cognito_id` varchar(255) NOT NULL COMMENT 'Cognito ID',
     `name` varchar(255) NOT NULL COMMENT '氏名',
+    `password` varchar(255) NOT NULL COMMENT 'パスワード',
     `email` varchar(255) NOT NULL COMMENT 'メールアドレス',
     `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '作成日',
     `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新日',
@@ -20,7 +21,8 @@ CREATE TABLE `d_posts`(
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 DROP TABLE IF EXISTS `m_good`;
-CREATE TABLE `m_good`(
+DROP TABLE IF EXISTS `d_good`;
+CREATE TABLE `d_good`(
     `post_id` bigint NOT NULL COMMENT '投稿ID',
     `user_id` int NOT NULL COMMENT 'ユーザID',
     PRIMARY KEY (`post_id`, `user_id`)
--- db/sql/02_insert_data.sql
+++ db/sql/02_insert_data.sql
@@ -1,14 +1,14 @@
 LOCK TABLES `m_users` WRITE;
-INSERT INTO webappdb.m_users (user_cognito_id,name,email,created_at,updated_at) VALUES
-	 ('xxx','Sashimi','sashimi@gmail.com','2022-02-18 18:07:47','2022-02-18 18:07:47');
+INSERT INTO webappdb.m_users (user_cognito_id,name,password,email,created_at,updated_at) VALUES
+	 ('xxx','Sashimi','test','sashimi@gmail.com','2022-02-18 18:07:47','2022-02-18 18:07:47');
 UNLOCK TABLES;
 
-LOCK TABLES `d_posts` WRITE:
+LOCK TABLES `d_posts` WRITE;
 INSERT INTO webappdb.d_posts (user_id,contents,created_at) VALUES
 	 (1,'contet-txt','2022-02-18 18:08:08');
 UNLOCK TABLES;
 
-LOCK TABLES `m_good` WRITE:
+LOCK TABLES `d_good` WRITE;
 INSERT INTO webappdb.m_good (post_id, user_id) VALUES
 	 (1,1);
 UNLOCK TABLES;
